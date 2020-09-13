76d9538062a1dd628e0c1ba0525019025a74dfd4
yueh <yueh@users.noreply.github.com>
2020-09-13 22:19:01 +0200

    Another try

--- api-integration-test/src/apionly/java/io/github/appliedenergistics/metrics/MicrometerClassAvailabilityTest.java
+++ api-integration-test/src/apionly/java/io/github/appliedenergistics/metrics/MicrometerClassAvailabilityTest.java
@@ -33,7 +33,7 @@ public class MicrometerClassAvailabilityTest {
 
         // assert that it is actually from our shaded jar and not some accidental
         // transitive dependency
-        assertTrue(filename.startsWith("metrics-api-"), "Expected name to start with metrics-api-: " + filename);
+        assertTrue(filename.startsWith("api-"), "Expected name to start with metrics-api-: " + filename);
         assertTrue(filename.endsWith(".jar"), "Expected name to end with .jar:" + filename);
     }
 
