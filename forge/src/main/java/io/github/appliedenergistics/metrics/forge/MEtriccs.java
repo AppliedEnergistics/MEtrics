0df32b126e04427f752dc2c4035f5145160bd97c
yueh <yueh@users.noreply.github.com>
2020-09-13 17:50:21 +0200

    Added forge mod
    Forge patches the fabric entry point away, so now we inject it after CrashReport is initialized

---
+++ forge/src/main/java/io/github/appliedenergistics/metrics/forge/MEtriccs.java
@@ -0,0 +1,24 @@
+package io.github.appliedenergistics.metrics.forge;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("metrics")
+public class MEtriccs {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public MEtriccs() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+}
---
+++ forge/src/main/java/io/github/appliedenergistics/metrics/forge/mixin/startup/ClientStartupMixin.java
@@ -0,0 +1,42 @@
+package io.github.appliedenergistics.metrics.forge.mixin.startup;
+
+import java.io.File;
+import java.nio.file.Path;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import io.github.appliedenergistics.metrics.core.Startup;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+import net.minecraft.client.main.Main;
+import net.minecraft.util.registry.Bootstrap;
+
+/**
+ * This mixin will run right before {@link Bootstrap#initialize()} does, and
+ * captures the game directory to initialize the metrics subsystems before any
+ * constructors are run.
+ */
+@Mixin(Main.class)
+public class ClientStartupMixin {
+
+    @Inject(method = "main", at = @At(value = "INVOKE", target = "Lnet/minecraft/crash/CrashReport;func_230188_h_()V", shift = Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
+    private static void beforeInitialize(String[] args, CallbackInfo ci, OptionParser optionParser) {
+        // The client uses this option to determine where the game is
+        OptionSpec<File> gameDirArg = optionParser.accepts("gameDir").withRequiredArg().ofType(File.class)
+                .defaultsTo(new File("."));
+
+        // We have to reparse the args, otherwise we'd need to capture all the options
+        // locally, which sucks
+        OptionSet optionSet = optionParser.parse(args);
+        Path gameDir = optionSet.valueOf(gameDirArg).toPath().toAbsolutePath();
+
+        Startup.initialize(gameDir, true);
+    }
+
+}
